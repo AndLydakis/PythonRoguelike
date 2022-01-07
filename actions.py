359ad82a716f3ade8f9505d27c42c7fd2e667bb2
AndLyd <andlydakis@gmail.com>
2022-01-07 19:27:22 +0100

    tiles/map

--- actions.py
+++ actions.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python3
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from engine import Engine
+    from entity import Entity
+
 
 class Action:
-    pass
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        """Perform this action with the objects needed to determine its scope
+        `engine` is the scope this action is performed in
+        `entity` is the object performing the action
+        Must be overriden by action subclasses
+        """
+        raise NotImplementedError()
 
 
 class EscapeAction(Action):
-    pass
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        raise SystemExit()
 
 
 class MovementAction(Action):
@@ -13,3 +27,13 @@ class MovementAction(Action):
         super().__init__()
         self.dx = dx
         self.dy = dy
+
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        dest_x = entity.x + self.dx
+        dest_y = entity.y + self.dy
+
+        if not engine.game_map.in_bounds(dest_x, dest_y):
+            return  # Destination out of bounds
+        if not engine.game_map.tiles['walkable'][dest_x, dest_y]:
+            return  # Destination blocked by tile
+        entity.move(self.dx, self.dy)
