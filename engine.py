53ea7089c348948fcaef215316d71ccdb592ec9b
AndLyd <andlydakis@gmail.com>
2022-01-22 21:37:05 +0100

    hp bar

--- engine.py
+++ engine.py
@@ -1,44 +1,51 @@
-from typing import Iterable, Any
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
 from tcod.context import Context
 from tcod.console import Console
 from tcod.map import compute_fov
 
-from entity import Entity
-from game_map import GameMap
-from input_handlers import EventHandler
+from input_handlers import MainGameEventHandler
+from render_functions import render_bar
+
+if TYPE_CHECKING:
+    from entity import Actor
+    from game_map import GameMap
+    from input_handlers import EventHandler
 
 
 class Engine:
-    def __init__(self, event_handler: EventHandler, game_map: GameMap, player: Entity):
-        self.event_handler = event_handler
-        self.game_map = game_map
+    game_map: GameMap
+
+    def __init__(self, player: Actor):
+        self.event_handler: EventHandler = MainGameEventHandler(self)
         self.player = player
-        self.update_fov()  # Update the FOV before the players next action.
 
     def handle_enemy_turns(self) -> None:
-        for entity in self.game_map.entities - {self.player}:
-            print(f'The {entity.name} wonders when it will get to take a real turn.')
-            
-    def handle_events(self, events: Iterable[Any]) -> None:
-        for event in events:
-            action = self.event_handler.dispatch(event)
-            if action is None:
-                continue
-
-            action.perform(self, self.player)
-            self.update_fov()
-
-    def render(self, console: Console, context: Context) -> None:
-        self.game_map.render(console=console)
-        context.present(console)
-        console.clear()
+        for entity in set(self.game_map.actors) - {self.player}:
+            if entity.ai:
+                entity.ai.perform()
 
     def update_fov(self) -> None:
-        """Recompute the visible area based on the player's FOV"""
+        """Recompute the visible area based on the players point of view."""
         self.game_map.visible[:] = compute_fov(
             self.game_map.tiles["transparent"],
             (self.player.x, self.player.y),
             radius=8,
         )
-        # If a tile is visible, it should be added to explored
+        # If a tile is "visible" it should be added to "explored".
         self.game_map.explored |= self.game_map.visible
+
+    def render(self, console: Console, context: Context) -> None:
+        self.game_map.render(console)
+        render_bar(
+            console=console,
+            current_value=self.player.fighter.hp,
+            maximum_value=self.player.fighter.max_hp,
+            total_width=20
+        )
+
+        context.present(console)
+
+        console.clear()
