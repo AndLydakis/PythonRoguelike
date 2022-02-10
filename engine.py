9b60758d4ce00530b86026314a4d58a1c727dd1d
AndLyd <andlydakis@gmail.com>
2022-02-10 19:45:33 +0100

    saving and loading

--- engine.py
+++ engine.py
@@ -1,35 +1,39 @@
 from __future__ import annotations
+
+import lzma
+import pickle
 from typing import TYPE_CHECKING
+
 from tcod.console import Console
 from tcod.map import compute_fov
-from input_handlers import MainGameEventHandler
-from message_log import MessageLog
-from render_functions import render_bar, render_names_at_mouse_location
+
 import exceptions
+from message_log import MessageLog
+from render_functions import (
+    render_bar,
+    render_names_at_mouse_location,
+)
 
 if TYPE_CHECKING:
     from entity import Actor
     from game_map import GameMap
-    from input_handlers import EventHandler
-    from components.consumable import Consumbale
 
 
 class Engine:
     game_map: GameMap
 
     def __init__(self, player: Actor):
-        self.event_handler: EventHandler = MainGameEventHandler(self)
         self.message_log = MessageLog()
-        self.player = player
         self.mouse_location = (0, 0)
+        self.player = player
 
     def handle_enemy_turns(self) -> None:
         for entity in set(self.game_map.actors) - {self.player}:
             if entity.ai:
                 try:
                     entity.ai.perform()
-                except exceptions.Impossible as ie:
-                    pass
+                except exceptions.Impossible:
+                    pass  # Ignore impossible action exceptions from AI.
 
     def update_fov(self) -> None:
         """Recompute the visible area based on the players point of view."""
@@ -43,11 +47,20 @@ class Engine:
 
     def render(self, console: Console) -> None:
         self.game_map.render(console)
+
         self.message_log.render(console=console, x=21, y=45, width=40, height=5)
+
         render_bar(
             console=console,
             current_value=self.player.fighter.hp,
             maximum_value=self.player.fighter.max_hp,
-            total_width=20
+            total_width=20,
         )
+
         render_names_at_mouse_location(console=console, x=21, y=44, engine=self)
+
+    def save_as(self, filename: str) -> None:
+        """Save this Engine instance as a compressed file."""
+        save_data = lzma.compress(pickle.dumps(self))
+        with open(filename, "wb") as f:
+            f.write(save_data)
